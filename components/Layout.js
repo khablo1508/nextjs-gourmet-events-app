35adeb381249be24f030d578212fb7e857b7b871
khablo1508 <khablo.anna@gmail.com>
2023-02-23 01:03:21 +0100

    create showcase component for home page

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,12 @@
+import { useRouter } from 'next/router';
 import Head from 'next/head';
 import Header from './Header';
 import Footer from './Footer';
+import Showcase from './Showcase';
 import styles from '@/styles/Layout.module.css';
 
 export default function Layout({ title, keywords, description, children }) {
+  const router = useRouter();
   return (
     <div>
       <Head>
@@ -11,8 +14,13 @@ export default function Layout({ title, keywords, description, children }) {
         <meta name='descrption' content={description} />
         <meta name='keywords' content={keywords} />
       </Head>
+
       <Header />
+
+      {router.pathname === '/' && <Showcase />}
+
       <div className={styles.container}>{children}</div>
+
       <Footer />
     </div>
   );
