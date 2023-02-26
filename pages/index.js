f489a24c0df7f5aaedc72631c9fee77d73925eb9
khablo1508 <khablo.anna@gmail.com>
2023-02-26 19:31:39 +0100

    create sigle event component, limit event list on the homepage, add events list to the page for all events

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,33 @@
+import Link from 'next/link';
 import Layout from '@/components/Layout';
+import EventItem from '@/components/EventItem';
 import { API_URL } from '@/config/index';
 
-export default function HomePage() {
+export default function HomePage({ events }) {
   return (
     <Layout>
       <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+
+      {events.length > 0 && (
+        <Link href='/events'>
+          <button className='btn-secondary'>View all events</button>
+        </Link>
+      )}
     </Layout>
   );
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const res = await fetch(`${API_URL}/api/events`);
   const events = await res.json();
 
-  console.log(events);
-
   return {
-    props: {},
+    props: { events: events.slice(0, 3) },
+    revalidate: 1,
   };
 }
